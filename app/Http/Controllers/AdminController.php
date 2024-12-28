2330bc6284d1a0e082019b6463f49adeb428dcb7
Marcelo Mesquita <marceloguilherme2010@gmail.com>
2024-12-27 23:30:05 -0300

    Feat: implementing tests to admin routes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,12 +11,9 @@ use Illuminate\Support\Facades\Log;
 
 class AdminController extends Controller
 {
-    private AdminService $adminService;
-
-    public function __construct(AdminService $adminService)
-    {
-        $this->adminService = $adminService;
-    }
+    public function __construct(
+        private AdminService $adminService
+    ) { }
 
     /**
      * Display a listing of the resource.
@@ -57,51 +54,6 @@ class AdminController extends Controller
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
-    {
-        $this->adminService->deleteAdmin($id);
-        return success_response(null, null, Response::HTTP_NO_CONTENT);
-    }
-
-    /**
-     * Display all arenas by adminId
-     */
-    public function arenas(Request $request)
-    {
-        $arenas = $this->adminService->getArenas($request);
-        return success_response($arenas);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function storeArena(Request $request)
-    {
-        $arena = $this->adminService->createArena($request);
-        return success_response($arena, null, Response::HTTP_CREATED);
-    }
-
-        /**
-     * Display the specified resource.
-     */
-    public function showArena(string $id)
-    {
-        $admin = $this->adminService->getAdminById($id);
-        return success_response($admin);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function updateArena(Request $request, string $id)
-    {
-        $admin = $this->adminService->updateAdmin($request, $id);
-        return success_response($admin);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroyArena(string $id)
     {
         $this->adminService->deleteAdmin($id);
         return success_response(null, null, Response::HTTP_NO_CONTENT);
